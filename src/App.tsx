6d1274e21bff3ae2f575d5e939f070442dcb5384
Oryngali <oryngali96@gmail.com>
2023-03-15 02:21:13 +0500

    feat: configure react-router, add not found, home, about us pages templates

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import classes from './App.module.scss';
+
+import About from './pages/about/About';
+import NotFound from './pages/notfound/NotFound';
+import Home from './pages/home/Home';
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <Link to="/">Home</Link>
+        <Link to="/about">About Us</Link>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/notfound/NotFound.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function NotFound() {
+  return <div>Страница не найдена</div>;
+}
+
+export default NotFound;
