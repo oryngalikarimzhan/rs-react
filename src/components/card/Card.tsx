3f6e0967acc7953b5eada3a046b7ec6531dc2819
Oryngali <oryngali96@gmail.com>
2023-03-17 15:51:04 +0500

    fix: change approach of working with data

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
-import { Character } from '../../types';
+import type { Character } from '../../types';
 import styles from './card.module.scss';
 
-export default class Card extends React.Component<Character, { hovered: boolean }> {
-  constructor(props: Character) {
+export default class Card extends React.Component<{ character: Character }, { hovered: boolean }> {
+  constructor(props: { character: Character }) {
     super(props);
     this.state = { hovered: false };
   }
 
   render() {
-    const character = this.props as Character;
+    const character = this.props.character;
+    const cardStyle = {
+      background: `url(${character.image}) no-repeat`,
+      backgroundSize: `${this.state.hovered ? 'auto 130%' : 'auto 100%'}`,
+      backgroundPosition: `${this.state.hovered ? 'left center' : 'center center}'}`,
+    };
+
     return (
       <div
         className={styles.card}
         onMouseOut={() => this.setState({ hovered: false })}
         onMouseOver={() => this.setState({ hovered: true })}
-        style={{
-          background: `url(${character.image}) no-repeat`,
-          backgroundSize: `${this.state.hovered ? 'auto 130%' : 'auto 100%'}`,
-          backgroundPosition: `${this.state.hovered ? 'left center' : 'center center}'}`,
-        }}
+        style={cardStyle}
       >
         <div className={styles.border}>
           <div className={styles.title}>{character.name}</div>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,15 +2,25 @@ import React from 'react';
 import Card from '../../components/card/Card';
 import styles from './home.module.scss';
 import data from '../../data/data.json';
-import marvel from '../../../public/Marvel_Logo.svg';
+import marvel from '../../assets/Marvel_Logo.svg';
+import type { Character } from '../../types';
 
-class Home extends React.Component {
+class Home extends React.Component<{ datas: Character[]; image: string }> {
+  state = {
+    datas: data.characters as Character[],
+    image: marvel,
+  };
+
   render(): React.ReactNode {
-    const cards = data.characters.map((character) => <Card key={character.name} {...character} />);
+    const cards = this.state.datas.map((characterData) => (
+      <Card key={characterData.name} character={characterData} />
+    ));
     return (
       <section className={styles.home}>
-        <img className={styles.logo} src={marvel} />
-        <div className={styles.cards}>{cards}</div>
+        <article className={styles.cardsContainer}>
+          <img className={styles.logo} src={this.state.image} />
+          <div className={styles.cards}>{cards}</div>
+        </article>
       </section>
     );
   }
